3576719faa54206b1bc43833ae85cc5e5543efae
thiagomedina <thiagomedina001@gmail.com>
2020-06-14 19:47:14 -0300

    banner

--- src/view/home/index.js
+++ src/view/home/index.js
@@ -1,68 +1,74 @@
 import React, { useState, useEffect } from 'react';
 import './home.css';
-import {Link} from 'react-router-dom';
+import img from '../../assets/banner.png'
+
 import Navbar from '../../components/navbar/';
 import { useSelector } from 'react-redux';
 import firebase from '../../config/firebase';
 import EventoCard from '../../components/card'
 
-function Home({match}){
+function Home({ match }) {
 
     const [evento, setEvento] = useState([]);
     const [pesquisa, setPesquisa] = useState('');
-    let listaProjetos = []; 
+    let listaProjetos = [];
     const usuarioEmail = useSelector(state => state.usuarioEmail);
 
     useEffect(() => {
 
-    if(match.params.parametro){
-        firebase.firestore().collection('eventos').where('usuario', '==', usuarioEmail).get()
-            .then(async (resultado) => {
-            await resultado.docs.map(doc => {
-                console.log(doc)
-                listaProjetos.push({
-                    id: doc.id,
-                    ...doc.data()
-                })
-           })
-   
-            setEvento(listaProjetos);
-       });
-       
-    }else{
-        firebase.firestore().collection('eventos').get()
-            .then(async (resultado) => {
-                await resultado.docs.map(doc => {
-                
-                    listaProjetos.push({
-                        id: doc.id,
-                        ...doc.data()
+        if (match.params.parametro) {
+            firebase.firestore().collection('eventos').where('usuario', '==', usuarioEmail).get()
+                .then(async (resultado) => {
+                    await resultado.docs.map(doc => {
+                        console.log(doc)
+                        listaProjetos.push({
+                            id: doc.id,
+                            ...doc.data()
+                        })
                     })
-        
-           })
-   
-           setEvento(listaProjetos);
-       });
-    }
-           
 
-});
+                    setEvento(listaProjetos);
+                });
+
+        } else {
+            firebase.firestore().collection('eventos').get()
+                .then(async (resultado) => {
+                    await resultado.docs.map(doc => {
+
+                        listaProjetos.push({
+                            id: doc.id,
+                            ...doc.data()
+                        })
+
+                    })
+
+                    setEvento(listaProjetos);
+                });
+        }
 
 
-    return(
+    });
+
+
+    return (
         <>
-        <Navbar/>
+            <Navbar />
 
-        <div className="row p-3 ">
-            <h2 className="mx-auto p-5">Projetos Publicados</h2>
-            {/* <input onChange={(e) => setPesquisa(e.target.value)} type="text" className="form-control text-center" placeholder="Pesquisar evento pelo título..." /> */}
-        </div>
-        
+            <div>
+                
+             <img  className="size "src={img}></img>
+            </div>
 
-        <div className="row p-3">
-        {evento.map(item => <EventoCard key={item.id} id={item.id} procura={item.procura} titulo={item.titulo} detalhes={item.detalhes} visualizacoes={item.visualizacoes}   />) }
+            <div className="row p-3 ">
+                <h2 className="mx-auto p-5">PROJETOS PUBLICADOS</h2>
+                {/* <input onChange={(e) => setPesquisa(e.target.value)} type="text" className="form-control text-center" placeholder="Pesquisar evento pelo título..." /> */}
+            </div>
 
-        </div>
+
+            <div className="row p-3">
+                {evento.map(item => <EventoCard key={item.id} id={item.id} procura={item.procura} titulo={item.titulo} detalhes={item.detalhes} visualizacoes={item.visualizacoes} />)}
+
+            </div>
         </>
     )
 }
